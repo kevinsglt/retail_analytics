8b12e26c72735fd463239c7d2c13fd8d71c694b1
kevinsglt <k.semsglt@gmail.com>
2025-10-04 04:12:07 +0200

    feat: add model int_orders_enriched

---
+++ models/intermediate/int_orders_enriched.sql
@@ -0,0 +1,30 @@
+{# int_orders_enriched.sql #}
+
+with stg_orders as (
+    select * from {{ ref('stg_orders') }}
+),
+
+stg_stores as (
+    select * from {{ ref('stg_stores') }}
+),
+
+joined as (
+
+    select
+
+        o.order_id,
+        o.customer_id,
+        o.store_id,
+        o.order_date,
+        o.net_amount,
+        o.payment_method,
+        s.store_type as channel
+
+    from stg_orders as o
+
+    left join stg_stores as s
+        on o.store_id = s.store_id
+
+)
+
+select * from joined
